bd7522f8bc6de08d89487a2b94b2489390588766
jhunter5 <jhunter@unal.edu.co>
2024-09-29 21:58:03 -0500

    refactor navbar

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,15 +1,21 @@
 import Link from "next/link"
 
+const navItems = [
+    { name: "Home", href: "#" },
+    { name: "About Us", href: "#" },
+    { name: "Projects", href: "#" },
+    { name: "Services", href: "#" },
+    { name: "Contact Us", href: "#" },
+]
+
 const NavBar = () => {
     return (
         <nav className="container mx-auto px-4 py-2 flex justify-between items-center">
           <div className="text-xl font-bold">NavItems</div>
           <ul className="flex space-x-4">
-            <li><Link href="#" className="hover:text-neutral-100">Home</Link></li>
-            <li><Link href="#" className="hover:text-neutral-100">About Us</Link></li>
-            <li><Link href="#" className="hover:text-neutral-100">Projects</Link></li>
-            <li><Link href="#" className="hover:text-neutral-100">Services</Link></li>
-            <li><Link href="#" className="hover:text-neutral-100">Contact Us</Link></li>
+            {navItems.map((data, index) => (
+                <li><Link href={data.href} className="hover:text-neutral-100">{data.name}</Link></li>
+            ))}
           </ul>
         </nav>
     )
